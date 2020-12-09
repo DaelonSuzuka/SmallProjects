f71995e8fd329d86b61d1fe024d3a99abf57de09
DaelonSuzuka <david@ldgelectronics.com>
2020-12-09 17:39:52 -0500

    Added printf wrapper

--- teensy-sd/src/main.cpp
+++ teensy-sd/src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
 
+
+extern "C" {
+#include <stdarg.h>
+int printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    char buf[100];
+
+    vsprintf(buf, format, args);
+
+    Serial.printf(buf);
+
+    va_end(args);
+    Serial.flush();
+    return 0;
+}
+#include "shell/shell.h"
+}
+
 void setup() {
-    //
     Serial.begin(9600);
+    shell_init();
 }
 
 void loop() {
-    Serial.println("Hello World...");
-    delay(1000);
+    while (1) {
+        shell_update((char)Serial.read());
+    }
 }
