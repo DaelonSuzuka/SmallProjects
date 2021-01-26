febc1377479e77bd5cb9f1b1ef5a9c0ece3c9cdc
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-01-25 21:21:31 -0500

    Rewrote firmware for genlink serial bridge

--- ESP32-bluetooth-serial/src/main.cpp
+++ ESP32-bluetooth-serial/src/main.cpp
@@ -1,20 +1,29 @@
 #include "BluetoothSerial.h"
+#include <HardwareSerial.h>
 #include <Arduino.h>
 
 BluetoothSerial BTserial;
+HardwareSerial HWserial(1);
 
 void setup() {
     Serial.begin(9600);
+    HWserial.begin(9600, SERIAL_8N1, 26, 27);
+
     BTserial.begin("H-100 Serial Bridge");
 }
 
 void loop() {
+    char c = 0;
     if (BTserial.available()) {
-        Serial.printf("%c", BTserial.read());
+        c = BTserial.read();
+        Serial.printf("BT: %c\r\n", c);
+        HWserial.printf("%c", c);
     }
 
-    if (Serial.available()) {
-        BTserial.printf("%c", Serial.read());
+    if (HWserial.available()) {
+        c = HWserial.read();
+        Serial.printf("HW: %c\r\n", c);
+        BTserial.printf("%c", c);
     }
     delay(20);
 }
